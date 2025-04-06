cba012981e299f51631ee35f4fa1495586fff0e1
odealidj <odealidj.go@gmail.com>
2025-04-07 04:08:55 +0700

    Add GetProductsHandler

---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,37 @@
+namespace Catalog.Products.Features.GetProducts;
+
+
+public record GetProductsQuery(string Category)
+    : IQuery<GetProductsResult>;
+public record GetProductsResult(IEnumerable<ProductDto> Products);
+
+public class GetProductsHandler(CatalogDbContext dbContext)
+    : IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+    {
+        // get products using dbContext
+        // return result
+
+        var products = await dbContext.Products
+            .AsNoTracking()
+            .Where(p => p.Category.Contains(query.Category))
+            .OrderBy(p => p.Name)
+            .ToListAsync(cancellationToken);
+        
+        //mapping product entity to productdto
+        var productDtos = ProjectToProductDto(products);
+
+        return new GetProductsResult(productDtos);
+    }
+
+    private List<ProductDto> ProjectToProductDto(List<Product> products)
+    {
+        foreach (var product in products)
+        {
+            
+        }
+
+        return [];
+    }
+}
